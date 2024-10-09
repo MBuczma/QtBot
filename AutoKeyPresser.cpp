29c8f7c96d2d6e0d9c510bf506df8659216c88fa
Mateusz Buczma <32811453+MBuczma@users.noreply.github.com>
2024-10-09 18:22:58 +0200

    Rozszerzenie mapy przycisków

--- AutoKeyPresser.cpp
+++ AutoKeyPresser.cpp
@@ -45,13 +45,21 @@ void AutoKeyPresser::WindowHandleFromPoint(HWND &handle, HWND &parentHandle)
 void AutoKeyPresser::SentKey(HWND &handle, QString key)
 {
     std::map<QString, WPARAM> keyMap = {
-        {"1", '1'},
-        {"2", '2'},
-        {"A", 'A'},
-        {"B", 'B'},
-        {"SPACE", VK_SPACE},
-        {"ENTER", VK_RETURN},
-        // Dodaj inne klawisze, które chcesz obsłużyć
+        {"Left", VK_LEFT}, {"Up", VK_UP},
+        {"Right", VK_RIGHT}, {"Down", VK_DOWN},
+        {"Space", VK_SPACE}, {"Enter", VK_RETURN},
+        {"Esc", VK_ESCAPE},
+        {"F1", VK_F1}, {"F2", VK_F2},
+        {"F3", VK_F3}, {"F4", VK_F4},
+        {"F5", VK_F5}, {"F6", VK_F6},
+        {"F7", VK_F7}, {"F8", VK_F8},
+        {"F9", VK_F9}, {"F10", VK_F10},
+        {"F11", VK_F11}, {"F12", VK_F12},
+        {"1", '1'}, {"2", '2'},
+        {"3", '3'}, {"4", '4'},
+        {"5", '5'}, {"6", '6'},
+        {"7", '7'}, {"8", '8'},
+        {"9", '9'}, {"0", '0'}
     };
 
     PostMessage(handle, WM_KEYDOWN, keyMap[key], 0);
