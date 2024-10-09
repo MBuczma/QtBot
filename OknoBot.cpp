3d9c398fa32e6dce9ca823dfc080c84747699640
Mateusz Buczma <32811453+MBuczma@users.noreply.github.com>
2024-10-09 18:49:50 +0200

    OknoBot unique pointer i refaktor

--- OknoBot.cpp
+++ OknoBot.cpp
@@ -1,74 +1,68 @@
 /* OknoBot.cpp */
 #include "OknoBot.h"
-#include "ui_OknoBot.h"
 #include <QMouseEvent>
 #include <QTimer>
+#include "ui_OknoBot.h"
+#include <memory>
 
 OknoBot::OknoBot(QWidget *parent)
     : QWidget(parent)
-    , ui(new Ui::OknoBot)
-    , autoKeyPresser(new AutoKeyPresser(this))
-    , handle(0)
-    , parentHandle(0)
-    , windowText("windowText")
-    , parentHandleWindowText("parentHandleWindowText")
-    , keyTimer(new QTimer(this))
-    , isButtonPressed(false) // Inicjalizacja timera
-    , countdownTimer(new QTimer(this))  // Timer do odliczania
+    , ui(std::make_unique<Ui::OknoBot>())
+    , autoKeyPresser(std::make_unique<AutoKeyPresser>(this))
+    , keyTimer(std::make_unique<QTimer>(this))
+    , countdownTimer(std::make_unique<QTimer>(this))
+    , isButtonPressed(false)
+    , isSendingKeys(false)
     , remainingTime(0)
-    , isSendingKeys(false)  // Początkowy stan: klawisze nie są wysyłane
 {
     ui->setupUi(this);
     connect(ui->pushButton_Start, &QPushButton::clicked, this, &OknoBot::start);
-    connect(ui->pushButton_DodajNowyRzad, &QPushButton::clicked, this, &OknoBot::dodajRzadPrzyciskow);
+    connect(ui->pushButton_DodajNowyRzad,
+            &QPushButton::clicked,
+            this,
+            &OknoBot::dodajRzadPrzyciskow);
     connect(ui->pushButton_PobierzID, &QPushButton::pressed, this, &OknoBot::ZlapIdOkna);
-    connect(keyTimer, &QTimer::timeout, this, &OknoBot::wyslijKlawisz);
-    connect(countdownTimer, &QTimer::timeout, this, &OknoBot::aktualizujCountdown);
+    connect(keyTimer.get(), &QTimer::timeout, this, &OknoBot::wyslijKlawisz);
+    connect(countdownTimer.get(), &QTimer::timeout, this, &OknoBot::aktualizujCountdown);
 }
 
-OknoBot::~OknoBot()
-{
-    delete keyTimer;
-    delete countdownTimer;
-    delete autoKeyPresser;
-    delete ui;
-}
+OknoBot::~OknoBot() = default;
 
 void OknoBot::start()
 {
-    qDebug() << "Przycisk start z oknobot został naciśnięty.";
-    qDebug() << "lineEdit_HotKey" <<  ui->lineEdit_HotKey->displayText();
-    qDebug() << "comboBox_Klawisz" << ui->comboBox_Klawisz->currentText();
-    qDebug() << "spinBox_Sekund" << ui->spinBox_Sekund->text();
-    qDebug() << "spinBox_Milisekund" << ui->spinBox_Milisekund->text();
-    qDebug() << "spinBox_WysleZa" << ui->spinBox_WysleZa->text();
-    //autoKeyPresser->SentKey(handle, ui->lineEdit_HotKey->displayText());
+    qDebug() << "Przycisk start z OknoBot został naciśnięty.";
+
     int czasSekund = ui->spinBox_Sekund->value();
     int czasMilisekund = ui->spinBox_Milisekund->value();
     QString wybranyKlawisz = ui->comboBox_Klawisz->currentText();
 
-    // Sprawdzenie, czy czas jest różny od 0 i klawisz został wybrany
     if ((czasSekund != 0 || czasMilisekund != 0) && !wybranyKlawisz.isEmpty()) {
-        if (!isSendingKeys) {
-            // Przycisk w stanie "Start"
-            isSendingKeys = true;
-            ui->pushButton_Start->setText("Stop");
-            ui->pushButton_Start->setStyleSheet("background-color: red; color: white;");
+        isSendingKeys = !isSendingKeys;
+        aktualizujStanPrzyciskuStart(isSendingKeys);
 
-            int interval = czasSekund * 1000 + czasMilisekund;  // Przelicz na milisekundy
+        if (isSendingKeys) {
+            int interval = czasSekund * 1000 + czasMilisekund;
             remainingTime = interval;
 
-            keyTimer->start(interval);  // Uruchom timer
+            keyTimer->start(interval);
+            countdownTimer->start(100);
         } else {
-            // Przycisk w stanie "Stop"
-            isSendingKeys = false;
-            ui->pushButton_Start->setText("Start");
-            ui->pushButton_Start->setStyleSheet("background-color: green; color: white;");
-
-            keyTimer->stop();  // Zatrzymaj timer
+            keyTimer->stop();
+            countdownTimer->stop();
         }
     } else {
         qDebug() << "Czas musi być większy od 0 i klawisz musi być wybrany.";
+    }
+}
+
+void OknoBot::aktualizujStanPrzyciskuStart(bool isSending)
+{
+    if (isSending) {
+        ui->pushButton_Start->setText("Stop");
+        ui->pushButton_Start->setStyleSheet("background-color: red; color: white;");
+    } else {
+        ui->pushButton_Start->setText("Start");
+        ui->pushButton_Start->setStyleSheet("background-color: green; color: white;");
     }
 }
 
@@ -79,7 +73,8 @@ void OknoBot::dodajRzadPrzyciskow()
 
 void OknoBot::ZlapIdOkna()
 {
-    qDebug() << "\n" << "Metoda OknoBot::ZlapIdOkna() został wywołana.";
+    qDebug() << "\n"
+             << "Metoda OknoBot::ZlapIdOkna() została wywołana.";
     isButtonPressed = true;
     grabMouse();
     setCursor(Qt::CrossCursor);
@@ -87,7 +82,7 @@ void OknoBot::ZlapIdOkna()
 
 void OknoBot::mouseReleaseEvent(QMouseEvent *event)
 {
-    if (event->button() == Qt::LeftButton && isButtonPressed == true){
+    if (event->button() == Qt::LeftButton && isButtonPressed) {
         qDebug() << "OknoBot::mouseReleaseEvent.";
         isButtonPressed = false;
         autoKeyPresser->WindowHandleFromPoint(handle, parentHandle);
@@ -96,46 +91,38 @@ void OknoBot::mouseReleaseEvent(QMouseEvent *event)
         zaktualizujNazwe();
         ui->pushButton_PobierzID->setDown(false);
     }
+    QWidget::mouseReleaseEvent(event); // Dodano wywołanie funkcji bazowej
 }
 
 void OknoBot::zaktualizujNazwe()
 {
     windowText = autoKeyPresser->GetWindowTextFromHandle(handle);
     parentHandleWindowText = autoKeyPresser->GetWindowTextFromHandle(parentHandle);
-    if(windowText == parentHandleWindowText)
-    {
+    if (windowText == parentHandleWindowText) {
         ui->groupBoxPrzyciski->setTitle(parentHandleWindowText);
-    }else
-    {
+    } else {
         ui->groupBoxPrzyciski->setTitle(parentHandleWindowText + " " + windowText);
     }
 }
 
 void OknoBot::wyslijKlawisz()
 {
-    // Pobranie klawisza z comboBox_Klawisz
     QString klawisz = ui->comboBox_Klawisz->currentText();
-
     qDebug() << "Wysyłam klawisz:" << klawisz;
-
-    // Wysyłanie klawisza (tutaj dodaj kod do wysyłania klawisza w zależności od platformy)
     autoKeyPresser->SentKey(handle, klawisz);
     remainingTime = ui->spinBox_Sekund->value() * 1000;
 }
 
 void OknoBot::aktualizujCountdown()
 {
-    // Zmniejsz pozostały czas
     remainingTime -= 100;
 
     if (remainingTime <= 0) {
-        remainingTime = 0;  // Upewnij się, że czas nie jest mniejszy niż 0
+        remainingTime = 0;
     }
 
-    // Zaktualizuj wartość w spinBox_WysleZa
     ui->spinBox_WysleZa->setValue(remainingTime);
 
-    // Jeśli remainingTime wynosi 0, odśwież na nowo interwał
     if (remainingTime == 0) {
         remainingTime = ui->spinBox_Sekund->value() * 1000;
     }
--- OknoBot.h
+++ OknoBot.h
@@ -2,8 +2,10 @@
 #ifndef OKNOBOT_H
 #define OKNOBOT_H
 
-#include "AutoKeyPresser.h"
+#include <QTimer>
 #include <QWidget>
+#include "AutoKeyPresser.h"
+#include <memory>
 
 namespace Ui {
 class OknoBot;
@@ -23,23 +25,27 @@ private slots:
     void ZlapIdOkna();
     void zaktualizujNazwe();
 
-
 protected:
     void mouseReleaseEvent(QMouseEvent *event) override;
 
 private:
-    Ui::OknoBot *ui;
-    AutoKeyPresser *autoKeyPresser;
+    std::unique_ptr<Ui::OknoBot> ui;
+    std::unique_ptr<AutoKeyPresser> autoKeyPresser;
+    std::unique_ptr<QTimer> keyTimer;
+    std::unique_ptr<QTimer> countdownTimer;
+
     HWND handle;
     HWND parentHandle;
     QString windowText;
     QString parentHandleWindowText;
-    QTimer *keyTimer;  // Timer do wysyłania klawisza
-    bool isButtonPressed;
+
+    bool isButtonPressed = false; // Zmienna do przechowywania stanu przycisku
+    bool isSendingKeys = false;   // Określa, czy klawisze są aktualnie wysyłane
+    int remainingTime = 0;        // Przechowuje pozostały czas do wysłania klawisza
+
     void wyslijKlawisz();
-    QTimer *countdownTimer;  // Timer do odliczania czasu do wysyłki
-    int remainingTime;  // Zmienna przechowująca pozostały czas do wysłania
-    void aktualizujCountdown();  // Funkcja do aktualizowania odliczania
-    bool isSendingKeys;  // Zmienna określająca, czy klawisze są wysyłane
+    void aktualizujCountdown();
+    void aktualizujStanPrzyciskuStart(bool isSending);
 };
+
 #endif // OKNOBOT_H
