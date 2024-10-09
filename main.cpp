2d61332821acad7b07bcd5607a33cb7644643936
Mateusz Buczma <32811453+MBuczma@users.noreply.github.com>
2024-10-09 18:23:30 +0200

    Zmiana na uniqe pointer

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 /* main.cpp */
 #include "GlowneOkno.h"
 #include <QApplication>
+#include <QStyleFactory>
+#include <memory>  // std::unique_ptr
 
 int main(int argc, char *argv[])
 {
     QApplication application(argc, argv);
-    GlowneOkno glowneOkno;
-    glowneOkno.show();
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
+    std::unique_ptr<GlowneOkno> glowneOkno = std::make_unique<GlowneOkno>();
+    glowneOkno->show();
     return application.exec();
 }
