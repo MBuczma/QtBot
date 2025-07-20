3d74eaa2ee4ca8d53bbfd0869dd3f4986ca7e6ec
Mateusz Buczma <32811453+MBuczma@users.noreply.github.com>
2025-07-21 00:13:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,25 +15,41 @@
 
 #include <QApplication>
 #include <QDateTime>
+#include <QDir>
 #include <QPalette>
+#include <QStandardPaths>
 #include <QStyleFactory>
 #include <memory> // std::unique_ptr
 
+void ustawLoggera();
 void ustawStyl(QApplication &application);
 
 int main(int argc, char *argv[])
 {
-    QString log_file = "QtBot_log_" + QDateTime::currentDateTime().toString("yyyy-MM-dd") + ".txt";
-    initLogger(log_file);
-    qInfo() << "Uruchomiono aplikacje";
-
     QApplication application(argc, argv);
+
+    ustawLoggera();
     ustawStyl(application);
 
     std::unique_ptr<GlowneOkno> glowneOkno = std::make_unique<GlowneOkno>();
     glowneOkno->show();
 
     return application.exec();
+}
+
+void ustawLoggera()
+{
+    QCoreApplication::setApplicationName("QtBot");
+    QString logDir = QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation)
+                     + "/Logs";
+    QDir().mkpath(logDir);
+
+    QString timestamp = QDateTime::currentDateTime().toString("yyyy-MM-dd_HH-mm-ss");
+    QString logFile = logDir + "/QtBot_log_" + timestamp + ".txt";
+
+    initLogger(logFile);
+    qInfo() << "log_file to " << logFile;
+    qInfo() << "Uruchomiono aplikacje";
 }
 
 void ustawStyl(QApplication &application)
